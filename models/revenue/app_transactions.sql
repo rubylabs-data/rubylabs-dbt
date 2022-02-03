05f713f27929127599ec1a63d60fc11156d78667
d.jayatillake@rubylabs.com <d.jayatillake@rubylabs.com>
2022-02-03 14:28:25 +0000

    docs added for all non ephemeral models

--- models/revenue/app_transactions.sql
+++ models/revenue/app_transactions.sql
@@ -1,18 +1,27 @@
-    {{ config(materialized = 'table')}}
-    
-    select  MIN(date) OVER(PARTITION BY app_name, subscriber_id, processing) min_date
-    , date
-    , end_time
-    , subscriber_id, app_name
-    , {{platform_lookup('processing')}} as platform
-    , country
-    , plan_id
-    , currency, price, proceeds
-    , refunded_at
-    , is_refunded
-    , billing_issues_detected_at
-    , is_billing_issue
-    , MAX(cancelled_at) OVER(PARTITION BY app_name, subscriber_id, processing, plan_id, original_store_transaction_id) cancelled_at
-    , store_transaction_id, original_store_transaction_id
-    , is_auto_renewable, is_trial_period
-    from {{ref('app_transactions_agg_eph')}}
+{{ config(materialized = 'table')}}
+
+select
+    min(date) over (partition by app_name, subscriber_id, processing) min_date,
+    date,
+    end_time,
+    subscriber_id,
+    app_name,
+    {{ platform_lookup("processing") }} as platform,
+    country,
+    plan_id,
+    currency,
+    price,
+    proceeds,
+    refunded_at,
+    is_refunded,
+    billing_issues_detected_at,
+    is_billing_issue,
+    max(cancelled_at) over (
+        partition by
+            app_name, subscriber_id, processing, plan_id, original_store_transaction_id
+    ) cancelled_at,
+    store_transaction_id,
+    original_store_transaction_id,
+    is_auto_renewable,
+    is_trial_period
+from {{ ref("app_transactions_agg_eph") }}
